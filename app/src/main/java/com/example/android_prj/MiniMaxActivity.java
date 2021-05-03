ae2e36ce1ebc3e907a17884575bf0c6d8eb105a2
AnissSO <60034986+AnissSO@users.noreply.github.com>
2021-05-03 18:46:13 +0000

    MiniMax Algorithm

--- app/src/main/java/com/example/android_prj/MiniMaxActivity.java
+++ app/src/main/java/com/example/android_prj/MiniMaxActivity.java
@@ -17,9 +17,14 @@ public class MiniMaxActivity extends AppCompatActivity implements View.OnClickLi
     TextView playerOneScore, playerTwoScore;    // layout Score Board
     ImageView[] cases = new ImageView[9];
     Button reset;                               // layout reset button
+    public TextView text;                       //Maybe we're not going to use it
+    //public PerfectComputerLogic perfectLogic;   MINIMAX
     int playerOneScoreCount, playerTwoScoreCount;// score keeping variables
     boolean activeRound, activePlayer;          // iterate rounds btw player and computer
     int movesCounter;
+
+    // 0 for case free; 1 for case occupied by Player; 2 for case occupied by computer
+    int [] casesState = {0,0,0,0,0,0,0,0,0};
 
     public boolean playerTurn = false;
     public boolean playerWon = false;
@@ -64,9 +69,50 @@ public class MiniMaxActivity extends AppCompatActivity implements View.OnClickLi
 
     }
 
+    public void placeObject(int move) {
+        // get the chosen move View
+        View w = this.cases[move];
+        if(casesState[move] != 0){
+            return;
+        }
+
+        if(playerTurn){
+            w.setBackgroundResource(R.drawable.cross);
+            this.casesState[move] = 1; //occupied by player
+        }
+        //check(); // check if the game is over
+        if(playerWon && !draw)
+            text.setText("Player Won!");
+        else if(computerWon && !draw)
+            text.setText("Computer Won!");
+        else if(draw)
+            text.setText("It's a draw!");
+        else
+            changeTurn();
+    }
+
+    public void changeTurn() {
+        playerTurn = !playerTurn;
+        if(playerTurn)
+            text.setText("Player's Turn");
+        else
+            text.setText("Computer's Turn");
+    }
 
     @Override
     public void onClick(View v) {
+        int move;
+        // Player Move: Start
 
+        //get the Num of the case Clicked
+        String caseId = v.getResources().getResourceName(v.getId());
+        int caseNum = Integer.parseInt(caseId.substring(caseId.length()-1, caseId.length())) - 1;
+        // check if it's an empty case
+        if (this.casesState[caseNum] != 0){
+            return;
+        }
+        placeObject(caseNum);
     }
+
+
 }
