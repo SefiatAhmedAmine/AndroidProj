906620854723bc74819958f8359c1c46b6f0f974
SefiatAhmedAmine <56233799+SefiatAhmedAmine@users.noreply.github.com>
2021-05-06 14:39:18 +0000

    Revert "Hichambranchefinal"

--- app/src/main/java/com/example/android_prj/HorsLigne_menu.java
+++ app/src/main/java/com/example/android_prj/HorsLigne_menu.java
@@ -1,40 +1,28 @@
 package com.example.android_prj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HorsLigne_menu extends AppCompatActivity {
 
-    private Button horsLigne2;
-    private Button horsLigne;
+    /**
+     *  this activity is for hicham to complete
+     *  options:
+     *      player vs player
+     *      3 levels of player vs computer
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hors_ligne_menu);
 
-        this.horsLigne2 = (Button) findViewById(R.id.button2);
-        this.horsLigne = (Button) findViewById(R.id.button);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_hors_ligne_menu);
 
-        this.horsLigne2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToGame = new Intent(getApplicationContext(), twoplayers.class);
-                startActivity(goToGame);
-                finish();
-            };
-        });
-        this.horsLigne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToGame = new Intent(getApplicationContext(), Robot_levels.class);
-                startActivity(goToGame);
-                finish();
-            };
-        });
+
     }
 }
